459a98378adcb8211379216608615bbb49aa2c6d
Olek <alexlikenew@gmail.com>
2024-04-10 12:07:29 +0200

    Minor js changes

--- WORK/liphoto/js/script.js
+++ WORK/liphoto/js/script.js
@@ -1,3 +1,3 @@
 const button = document.querySelector('button')
 const wrapper = document.querySelector('.wrapper')
-const bidy = document.querySelector('body')
+const body = document.querySelector('body')
