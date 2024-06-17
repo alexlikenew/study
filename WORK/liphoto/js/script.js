b1daa1af128057b2da22ad9218aa0e4c3ab71429
Olek <alexlikenew@gmail.com>
2024-06-17 11:21:24 +0200

    added js elements

--- WORK/liphoto/js/script.js
+++ WORK/liphoto/js/script.js
@@ -1,3 +1,4 @@
 const header = document.querySelector('header')
 const main = document.querySelector('main')
 const footer = document.querySelector('footer')
+const elementImge = document.querySelector('.elements__element__image')
